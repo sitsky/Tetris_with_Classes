80139067c7df34d8e4d42542dd02bc00a42fcbae
sitsky <aris.miliotis@gmail.com>
2017-10-18 15:21:13 -0400

    Initial commit

---
+++ Assets/Main.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+
+
+    public List<Block> Active_Blocks;
+	// Use this for initialization
+	void Start () {
+        Active_Blocks.Add(new Block());       
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Active_Blocks[0].move_left();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Active_Blocks[0].move_right();
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Active_Blocks[0].rotate_clockwise();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Active_Blocks[0].rotate_anticlockwise();
+        }
+    }
+}
