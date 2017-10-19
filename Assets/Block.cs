a8ec375fc311d2684be2d7fa6f6ccc9d96f1db30
sitsky <aris.miliotis@gmail.com>
2017-10-19 13:27:25 -0400

    Something wrong with rotaions. a block disappears...

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -4,13 +4,20 @@ using System.Collections;
 public class Block : MonoBehaviour {
 
     public enum Block_Shape : int { L = 1, J, I, O, S, Z, T };
+    public enum Block_Orientation : int { ZERO = 0, NINTY = 1, ONEEIGHTY = 2, TWOSEVENTY = 3 };
     public Vector2[] position = new Vector2[4];
-    public string myShape;
+    public Block_Shape myshape;
+    public Block_Orientation myorientation;
+
+    //public string myShape;
 
     public Block()
     {
+        myorientation = Block_Orientation.ZERO;
+        myshape = (Block_Shape)Random.Range(1, 7);
         position = create_Block(Block_Shape.L);
-        myShape = create_Block_string((Block_Shape)Random.Range(1, 7));
+
+        //myShape = create_Block_string(myshape);
     }
 
 
@@ -21,28 +28,52 @@ public class Block : MonoBehaviour {
         {
             case Block_Shape.L:
                 block_chosen[0] = new Vector2(0, 0);
-                block_chosen[1] = new Vector2(1, 1);
-                block_chosen[2] = new Vector2(2, 1);
-                block_chosen[3] = new Vector2(3, 1);
+                block_chosen[1] = new Vector2(0, -1);
+                block_chosen[2] = new Vector2(0, -2);
+                block_chosen[3] = new Vector2(1, -2);
                 break;
             case Block_Shape.J:
+                block_chosen[0] = new Vector2(0, 0);
+                block_chosen[1] = new Vector2(0, -1);
+                block_chosen[2] = new Vector2(0, -2);
+                block_chosen[3] = new Vector2(-1, -2);
                 break;
             case Block_Shape.I:
+                block_chosen[0] = new Vector2(0, 0);
+                block_chosen[1] = new Vector2(0, -1);
+                block_chosen[2] = new Vector2(0, -2);
+                block_chosen[3] = new Vector2(0, -3);
                 break;
             case Block_Shape.O:
+                block_chosen[0] = new Vector2(0, 0);
+                block_chosen[1] = new Vector2(0, -1);
+                block_chosen[2] = new Vector2(1, 0);
+                block_chosen[3] = new Vector2(1, -1);
                 break;
             case Block_Shape.S:
+                block_chosen[0] = new Vector2(0, 0);
+                block_chosen[1] = new Vector2(0, 1);
+                block_chosen[2] = new Vector2(1, 0);
+                block_chosen[3] = new Vector2(-1, -1);
                 break;
             case Block_Shape.T:
+                block_chosen[0] = new Vector2(0, 0);
+                block_chosen[1] = new Vector2(-1,0);
+                block_chosen[2] = new Vector2(1, 0);
+                block_chosen[3] = new Vector2(0, -1);
                 break;
             case Block_Shape.Z:
+                block_chosen[0] = new Vector2(0, 0);
+                block_chosen[1] = new Vector2(0, 1);
+                block_chosen[2] = new Vector2(0, -1);
+                block_chosen[3] = new Vector2(-1, -1);
                 break;
             default:
                 break;
         }
         return block_chosen;
     }
-    public string create_Block_string(Block_Shape make_shape)
+    /*public string create_Block_string(Block_Shape make_shape)
     {
         string block_chosen = "";
         switch(make_shape)
@@ -72,7 +103,7 @@ public class Block : MonoBehaviour {
                 break;
         }
         return block_chosen;
-    }
+    }*/
 
     public void move_left()
     {
@@ -93,28 +124,46 @@ public class Block : MonoBehaviour {
     {
         for (int part_of_block = 0; part_of_block < position.Length; part_of_block++)
         {
-            position[part_of_block] += Vector2.down;
+            position[part_of_block] += (Vector2.down) * 0.5f;
         }
     }
 
     public void rotate_clockwise()
     {
-        Vector2 pivot = position[0];
-        for (int part_of_block = 1; part_of_block > position.Length; part_of_block++)
+        Debug.Log("Block| myorient: " + myorientation.ToString());
+        myorientation++;
+        if (myorientation > Block_Orientation.TWOSEVENTY) myorientation = Block_Orientation.ZERO;
+
+        if (myshape == Block_Shape.O) return;
+        else
         {
-            Vector2 direction_of_part = position[part_of_block] - pivot;
-            direction_of_part = Quaternion.Euler(0, 0, 90) * direction_of_part;
-            position[part_of_block] = direction_of_part + pivot;
+            Vector2 pivot = position[0];
+            Debug.Log("pivot: " + pivot.ToString());
+            for (int part_of_block = 0; part_of_block < 4; part_of_block++)
+            {
+                Vector2 direction_of_part = position[part_of_block] - pivot;
+                direction_of_part = Quaternion.Euler(0, 0, -90) * direction_of_part;
+                position[part_of_block] = direction_of_part + pivot;
+
+                Debug.Log("originalpart: " + part_of_block.ToString());
+            }
         }
     }
     public void rotate_anticlockwise()
     {
-        Vector2 pivot = position[0];
-        for (int part_of_block = 1; part_of_block > position.Length; part_of_block++)
+        myorientation--;
+        if (myorientation > Block_Orientation.TWOSEVENTY) myorientation = Block_Orientation.ZERO;
+
+        if (myshape == Block_Shape.O) return;
+        else
         {
-            Vector2 direction_of_part = position[part_of_block] - pivot;
-            direction_of_part = Quaternion.Euler(0, 0, 90) * direction_of_part;
-            position[part_of_block] = direction_of_part + pivot;
+            Vector2 pivot = position[0];
+            for (int part_of_block = 1; part_of_block < position.Length; part_of_block++)
+            {
+                Vector2 direction_of_part = position[part_of_block] - pivot;
+                direction_of_part = Quaternion.Euler(0, 0, 90) * direction_of_part;
+                position[part_of_block] = direction_of_part + pivot;
+            }
         }
     }
 }
--- Assets/Main.cs
+++ Assets/Main.cs
@@ -37,6 +37,7 @@ public class Main : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            Debug.Log("Main.UP");
             Active_Blocks[Active_Blocks.Count - 1].rotate_clockwise();
         }
 
@@ -45,16 +46,19 @@ public class Main : MonoBehaviour {
             Active_Blocks[Active_Blocks.Count - 1].rotate_anticlockwise();
         }
 
-        Active_Blocks[Active_Blocks.Count - 1].move_down();
-
-        Debug.Log(Active_Blocks[Active_Blocks.Count - 1].position[0].ToString());
-        
-            for (int part_of_block = 0; part_of_block < Active_Blocks[Active_Blocks.Count - 1].position.Length; part_of_block++)
+        if (Active_Blocks.Count < 2)
         {
-            if (Active_Blocks[Active_Blocks.Count - 1].position[part_of_block].y < -100)
+            Active_Blocks[Active_Blocks.Count - 1].move_down();
+        }
+
+        //Debug.Log(Active_Blocks[Active_Blocks.Count - 1].position[0].ToString());
+        
+        for (int part_of_block = 0; part_of_block < Active_Blocks[Active_Blocks.Count - 1].position.Length; part_of_block++)
+        {
+            if (Active_Blocks[Active_Blocks.Count - 1].position[part_of_block].y < -8)
             {
                 Active_Blocks.Add(Next_Block);
-                Next_Block = new Block();
+                //Next_Block = new Block();
             }
 
         }
@@ -63,12 +67,36 @@ public class Main : MonoBehaviour {
 
     public void display()
     {
-        string the_game_view = "";
+        string to_text_box = "";
+
+        int[,] the_game_view = new int[20,40];
         foreach(Block block_in_game in Active_Blocks)
         {
-            the_game_view = block_in_game.myShape + "\n   \n" + the_game_view;
+            for(int part_of_block = 0; part_of_block < 4; part_of_block++)
+            {
+                //Debug.Log(position_part_of_block.x.ToString() + "  " + position_part_of_block.y.ToString());
+
+                int column = 5 + (int)block_in_game.position[part_of_block].x;
+                int row = 32 + (int)block_in_game.position[part_of_block].y;
+                the_game_view[column, row] = 1;
+                }
         }
-        text_box.text = the_game_view;
+        for (int line = 39; line >= 0; line--)
+        {
+            for (int column = 0; column < 20; column++)
+            {
+                if (the_game_view[column, line] == 1)
+                {
+                    to_text_box = to_text_box + "X";
+                }
+                else
+                {
+                    to_text_box = to_text_box + " ";
+                }
+            }
+            to_text_box = to_text_box + "\n";
+        }
+        text_box.text = to_text_box;
     }
 
 
